ba22d5d39e22d3c2d713094e459dd2345f4e5b65
Mateusz Maćkowski <m4tx@m4tx.pl>
2016-03-23 22:32:24 +0100

    Change temperature and humidity to float fields

--- api/models.py
+++ api/models.py
@@ -25,12 +25,10 @@ class Telemetry(TimestampModel):
     oxygen = models.IntegerField()
     ion_radiation = models.IntegerField(verbose_name='Ionizing radiation')
 
-    humidity = models.DecimalField(
-        max_digits=5, decimal_places=2,
+    humidity = models.FloatField(
         help_text='0 to 100%, resolution 0.04%')
 
-    temperature = models.DecimalField(
-        max_digits=5, decimal_places=2,
+    temperature = models.FloatField(
         help_text='-40 to 125℃, resolution 0.01℃')
 
     pressure = models.FloatField(help_text='Air pressure [hPa]')
